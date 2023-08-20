4973c6a93b43b487b331358cd80c80b5c3813dc8
menezescosta <mc611641@gmail.com>
2023-08-20 23:20:15 +0100

    chore: configurando o projectp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,268 @@
-import Image from 'next/image'
+import { FaIgloo } from "react-icons/fa";
+import {
+  AiOutlineLeft,
+  AiOutlineRight,
+  AiFillStepBackward,
+  AiFillStepForward,
+} from "react-icons/ai";
+import { BiLibrary, BiSearchAlt, BiShuffle } from "react-icons/bi";
+import { BsFillPlayFill } from "react-icons/bs";
+import { CiRepeat } from "react-icons/ci";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+          </div>
+
+          <nav className="space-y-5 mt-10">
+            <a
+              href=""
+              className="flex items-center  gap-2 text-xl font-semibold text-zinc-200"
+            >
+              <FaIgloo />
+              Home
+            </a>
+            <a
+              href=""
+              className="flex items-center  gap-2 text-xl font-semibold text-zinc-200"
+            >
+              <BiSearchAlt />
+              Search
+            </a>
+            <a
+              href=""
+              className="flex items-center  gap-2 text-xl font-semibold text-zinc-200"
+            >
+              <BiLibrary />
+              Your Library
+            </a>
+          </nav>
+
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="text-sm text-zinc-400 hover:text-zinc-100">
+              Banda Maravilha Angola
+            </a>
+            <a href="text-sm text-zinc-400 hover:text-zinc-100">
+              Groove Is The Future
+            </a>
+            <a href="text-sm text-zinc-400 hover:text-zinc-100">
+              Os Irmãos Calemas
+            </a>
+            <a href="text-sm text-zinc-400 hover:text-zinc-100">
+              Banda União Murro Bento
+            </a>
+            <a href="text-sm text-zinc-400 hover:text-zinc-100">
+              Geração Church
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-3">
+            <button className="rounded-full bg-black/40 p-2">
+              <AiOutlineLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-2">
+              <AiOutlineRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-4">Seja Bem vindo/a!</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-6">
+            <a
+              href="#"
+              className="bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image src="/dj.jpg" width={104} height={104} alt="" />
+              <strong>Rest in peace</strong>
+              <button className="p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image src="/dj.jpg" width={104} height={104} alt="" />
+              <strong>Rest in peace</strong>
+              <button className="p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image src="/dj.jpg" width={104} height={104} alt="" />
+              <strong>Rest in peace</strong>
+              <button className="p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image src="/dj.jpg" width={104} height={104} alt="" />
+              <strong>Rest in peace</strong>
+              <button className="p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image src="/dj.jpg" width={104} height={104} alt="" />
+              <strong>Rest in peace</strong>
+              <button className="p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/20 transition-colors"
+            >
+              <Image src="/dj.jpg" width={104} height={104} alt="" />
+              <strong>Rest in peace</strong>
+              <button className="p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-6">
+            Feito por Mauricio Costa!
+          </h2>
+
+          <div className="grid grid-cols-5 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 group p-3 rounded hover:bg-white/10 relative"
+            >
+              <Image
+                src="/dj.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt=""
+              />
+              <button className="absolute p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+              <strong className="font-semibold block">My first love</strong>
+              <span className="text-xs text-zinc-500">
+                Mauricio Costa, feat MC El Pidro Muteka & Hori Jesus
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group p-3 rounded hover:bg-white/10 relative"
+            >
+              <Image
+                src="/full.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt=""
+              />
+              <button className="absolute p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+              <strong className="font-semibold block">My first love</strong>
+              <span className="text-xs text-zinc-500">
+                Mauricio Costa, feat MC El Pidro Muteka & Hori Jesus
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group p-3 rounded hover:bg-white/10 relative"
+            >
+              <Image
+                src="/ai.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt=""
+              />
+              <button className="absolute p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+              <strong className="font-semibold block">My first love</strong>
+              <span className="text-xs text-zinc-500">
+                Mauricio Costa, feat MC El Pidro Muteka & Hori Jesus
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group p-3 rounded hover:bg-white/10 relative"
+            >
+              <Image
+                src="/medium.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt=""
+              />
+              <button className="absolute p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+              <strong className="font-semibold block">My first love</strong>
+              <span className="text-xs text-zinc-500">
+                Mauricio Costa, feat MC El Pidro Muteka & Hori Jesus
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group p-3 rounded hover:bg-white/10 relative"
+            >
+              <Image
+                src="/dj.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt=""
+              />
+              <button className="absolute p-2 rounded-full invisible group-hover:visible bg-green-400 text-black text-xl ml-auto mr-8">
+                <BsFillPlayFill />
+              </button>
+              <strong className="font-semibold block">My first love</strong>
+              <span className="text-xs text-zinc-500">
+                Mauricio Costa, feat MC El Pidro Muteka & Hori Jesus
+              </span>
+            </a>
+          </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-4 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/dj.jpg" width={60} height={60} alt="" />
+          <div className="flex flex-col">
+            <strong className="font-normal">Groove Is The Future</strong>
+            <span className="text-xs text-zinc-400">Mauricio Costa</span>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-6">
+            <BiShuffle size={20} className="text-zinc-200" />
+            <AiFillStepBackward size={20} className="text-zinc-200" />
+            <button className="p-2 rounded-full bg-white text-black text-xl">
+              <BsFillPlayFill size={20} className="" />
+            </button>
+            <AiFillStepForward size={20} className="text-zinc-200" />
+            <CiRepeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2"></div>
+        </div>
+        <div></div>
+      </footer>
+    </div>
+  );
 }
