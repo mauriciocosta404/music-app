97294713ee60f5370031752d94d32b02fae7cf84
mauriciocosta404 <mc611641@gmail.com>
2023-10-03 16:44:13 +0100

    feat: adicionando o component

--- src/components/musicItem.tsx
+++ src/components/musicItem.tsx
@@ -8,7 +8,7 @@ type Music={
 
 const MusicItem:React.FC<Music>=({musicProps})=>{
     return(
-        <li className="flex justify-between items-center border-b border-zinc-400 p-2">
+        <li className="flex justify-between items-center border-b border-zinc-400 p-2 cursor-pointer hover:bg-zinc-800 transition-colors">
               <span className="text-zinc-300">{musicProps.id}</span>
 
               <div className="max-w-6 h-full">
