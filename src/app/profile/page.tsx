562e1d23f8bedfa17a95cf69de17b8f2b01faaff
mauriciocosta404 <mc611641@gmail.com>
2024-03-15 15:06:43 +0000

    refactor: refactorando o codigo

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -45,85 +45,83 @@ import {
               </div>
             </div>
 
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-400">
+                    <thead className="text-xs uppercase bg-gray-800 text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Product name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Color
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Price
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">
+                                Silver
+                            </td>
+                            <td className="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td className="px-6 py-4">
+                                $2999
+                            </td>
+                            <td className="px-6 py-4">
+                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                        <tr className=" odd:bg-gray-900  even:bg-gray-800 border-b border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Google Pixel Phone
+                            </th>
+                            <td className="px-6 py-4">
+                                Gray
+                            </td>
+                            <td className="px-6 py-4">
+                                Phone
+                            </td>
+                            <td className="px-6 py-4">
+                                $799
+                            </td>
+                            <td className="px-6 py-4">
+                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple Watch 5
+                            </th>
+                            <td className="px-6 py-4">
+                                Red
+                            </td>
+                            <td className="px-6 py-4">
+                                Wearables
+                            </td>
+                            <td className="px-6 py-4">
+                                $999
+                            </td>
+                            <td className="px-6 py-4">
+                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             
-
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-400">
-        <thead className="text-xs uppercase bg-gray-800 text-gray-400">
-            <tr>
-                <th scope="col" className="px-6 py-3">
-                    Product name
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Color
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr className="border-border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td className="px-6 py-4">
-                    Silver
-                </td>
-                <td className="px-6 py-4">
-                    Laptop
-                </td>
-                <td className="px-6 py-4">
-                    $2999
-                </td>
-                <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr className=" odd:bg-gray-900  even:bg-gray-800 border-b border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Google Pixel Phone
-                </th>
-                <td className="px-6 py-4">
-                    Gray
-                </td>
-                <td className="px-6 py-4">
-                    Phone
-                </td>
-                <td className="px-6 py-4">
-                    $799
-                </td>
-                <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple Watch 5
-                </th>
-                <td className="px-6 py-4">
-                    Red
-                </td>
-                <td className="px-6 py-4">
-                    Wearables
-                </td>
-                <td className="px-6 py-4">
-                    $999
-                </td>
-                <td className="px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-
           </main>
         </div>
        </div>
